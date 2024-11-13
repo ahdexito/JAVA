ff0552eeef1d85cc1d6ec75a208964c57b313841
Angel <ags120120@gmail.com>
2024-11-13 09:12:15 +0100

    save

---
+++ TEMA 3/AUTOCORRECCIÓN/prog03_autoMatriz07/src/prog03_automatriz07/Prog03_autoMatriz07.java
@@ -0,0 +1,33 @@
+package prog03_automatriz07;
+
+import java.util.Scanner;
+
+public class Prog03_autoMatriz07 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		/*
+        7. Un centro escolar nos ha pedido mantener las notas de las 5 asignaturas que cursan los
+		alumnos. En cada asignatura puede haber un número distintos de alumnos:
+		a) Asignatura 1: 3 alumnos.
+		b) Asignatura 2: 5 alumnos.
+		c) Asignatura 3: 4 alumnos.
+		d) Asignatura 4: 2 alumnos.
+		e) Asignatura 5: 4 alumnos
+		Se debe mantener la información mediante una variable que gráficamente presenta esta
+		estructura:
+		
+		Crear una aplicación que permita:
+		a) Instar las notas de la asignatura seleccionada.
+		b) Calcular la nota media de la asignatura seleccionada.
+		c) Ordenar las notas de una asignatura de forma ascendente.
+		d) Estadísticas de porcentajes de aprobados y suspensos por cada asignatura.
+		e) Salir.
+		*/
+		
+		
+		
+	}
+
+}
