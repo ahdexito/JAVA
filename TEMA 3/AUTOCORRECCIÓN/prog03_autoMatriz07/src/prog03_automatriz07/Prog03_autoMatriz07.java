58c5e646e5cce37e240ebce003202267dd120335
Angel <ags120120@gmail.com>
2024-11-13 12:04:11 +0100

    save

--- TEMA 3/AUTOCORRECCIÓN/prog03_autoMatriz07/src/prog03_automatriz07/Prog03_autoMatriz07.java
+++ TEMA 3/AUTOCORRECCIÓN/prog03_autoMatriz07/src/prog03_automatriz07/Prog03_autoMatriz07.java
@@ -1,5 +1,6 @@
 package prog03_automatriz07;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Prog03_autoMatriz07 {
@@ -26,39 +27,79 @@ public class Prog03_autoMatriz07 {
 		e) Salir.
 		*/
 		
-		int[][] notas = new int[5][];
+		System.out.println("" +
+		"a) Instar las notas de la asignatura seleccionada.\n" +
+		"b) Calcular la nota media de la asignatura \n" +
+		"   seleccionada.\n" +
+		"c) Ordenar las notas de una asignatura de forma\n" +
+		"   ascendente.\n" +
+		"d) Estadísticas de porcentajes de aprobados \n" +
+		"   y suspensos por cada asignatura.\n" +
+		"e) Mostrar todas las notas.\n" +
+		"f) Salir.\n");
 		
-		notas[0] = new int[3];
-		notas[1] = new int[5];
-		notas[2] = new int[4];
-		notas[3] = new int[2];
-		notas[4] = new int[4];
+		int[][] notas = {{5, 6, 5},{3, 7, 8, 2, 1},{5, 6, 3, 7},{3, 6},{6, 7, 8, 3}};
 		
-		// Imprimir notas.
-		for (int i=0; i<5; i++) {
-			
-			System.out.print("ASIGNATURA " + (i+1) + ": ");
-			
-			for (int j=0; j<notas[i].length; j++) {
-				
-				System.out.print(notas[i][j] + "  ");
-			}
-			System.out.println("\n");
-		}
-		
+		char opcion;
+
 		do {
-			System.out.print("Introduce opción: ");
-			char opcion = sc.next().charAt(0);
+			double media = 0;
+			
+			System.out.println("==================================================\n");
+			System.out.print("Introduce opción (a, b, c, d, e, f): ");
+			opcion = sc.next().charAt(0);
 			
 			switch (opcion) {
 				
+				case 'a': case 'A':
+					
+				case 'b': case 'B':
+					
+					System.out.println("OPCIÓN B: MEDIA DE NOTAS\n");
+					System.out.print("Introduce asignatura (1, 2, 3, 4, 5): ");
+					int entrada_B = sc.nextInt() - 1;
+					
+					for (int i=0; i<notas[entrada_B].length; i++) {
+						
+						media += notas[entrada_B][i];
+					}
+					
+					System.out.printf("La media de notas de la asignatura %d es: %.1f", (entrada_B + 1), (media/notas[entrada_B].length));
+					System.out.println("\n");
+				break;
+					
+				case 'c': case 'C':
+					
+					System.out.println("OPCIÓN C: ORDENAR NOTAS\n");
+					System.out.print("Introduce asignatura (1, 2, 3, 4, 5): ");
+					int entrada_C = sc.nextInt() - 1;
+					
+					Arrays.sort(notas[entrada_C]);
+					
+					System.out.println("\nNotas de la asignatura " + (entrada_C + 1) + " ordenadas.\n");
+				break;
+					
+				case 'e': case 'E':
+					
+					System.out.println("OPCIÓN E: MOSTRAR NOTAS\n");
+					for (int i=0; i<5; i++) {
+			
+						System.out.print("ASIGNATURA " + (i+1) + ": ");
+
+						for (int j=0; j<notas[i].length; j++) {
+
+							System.out.print(notas[i][j] + "  ");
+						}
+						System.out.println("\n");
+					}
+				break;
 				
-				
+				case 'f': case 'F':
+					
+					System.out.println("\nOPCIÓN F: SALIR DEL PROGRAMA");
+				break;
 			}
 			
-			
-		} (while opcion!='e' || opcion!='E')
-		
+		} while (opcion!='f' && opcion!='F');
 	}
-
 }
