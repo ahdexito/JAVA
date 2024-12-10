47693b2faccd2d851f9a30fda326fb70815bec4d
Angel <ags120120@gmail.com>
2024-12-10 12:00:51 +0100

    save

--- TEMA 4/AUTOCORECCIÓN/prog04_auto01/src/prog04_auto01/Prog04_auto01.java
+++ TEMA 4/AUTOCORECCIÓN/prog04_auto01/src/prog04_auto01/Prog04_auto01.java
@@ -274,12 +274,26 @@ public class Prog04_auto01 {
 		return contador;
 	}
 	
-	public static int[] ArrayCuadrado(int[] array) {
+	public static void ArrayCuadrado(int[] array) {
 		
+		for (int i=0; i<array.length; i++) {
+			
+			array[i] = (int)Math.pow(array[i], 2);
+		}
 		
 	}
 	
-	
+	public static void DiagonalPrincipal(int[][] matriz) {
+		
+		for (int i=0; i<matriz.length; i++) {
+			
+			for (int j=0; j<matriz[i].length; j++) {
+			
+				if (i == j) matriz[i][j] = 1;
+				else matriz[i][j] = 0;
+			}
+		}
+	}
 	
 	
 	
@@ -436,9 +450,9 @@ public class Prog04_auto01 {
 //		*/
 //		
 //		System.out.print("Introduce tamaño del array: ");
-//		entrada1 = sc.nextInt();
+//		entrada = sc.nextInt();
 //		
-//		int[] array = new int[entrada1];
+//		int[] array = new int[entrada];
 //		
 //		int[] arrayLleno = ArrayAleatorio(array);
 //		
@@ -579,15 +593,34 @@ public class Prog04_auto01 {
 //		}
 //		
 //		System.out.println("Has acertado: " + AciertosApuesta(tablaAdivinar, tablaGanadora));
+//		
+//		/*
+//		25. Crear una función llamada ArrayCuadrado, que reciba un array de enteros de 10 elementos,
+//		devuelva el mismo array pero elevando al cuadrado los valores del array. Emplea el paso de
+//		parámetros por referencia
+//		*/
+//		
+//		int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//		
+//		ArrayCuadrado(array);
+//		
+//		System.out.println(Arrays.toString(array));
+//		
 		
 		/*
-		25. Crear una función llamada ArrayCuadrado, que reciba un array de enteros de 10 elementos,
-		devuelva el mismo array pero elevando al cuadrado los valores del array. Emplea el paso de
-		parámetros por referencia
+		27. Crear una función llamada DiagonalPrincipal, que reciba una matriz de 7x7 la rellene de
+		forma que los elementos de la diagonal principal sean 1 y el resto 0, y la devuelva por el
+		paso de parámetros por referencia.
 		*/
 		
-		int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		int[][] matriz = new int[7][7];
 		
-		System.out.println(Arrays.toString(ArrayCuadrado(array)));
+		DiagonalPrincipal(matriz);
+		
+		for (int i=0; i<matriz.length; i++) {
+			
+			System.out.println(Arrays.toString(matriz[i]));
+		}
+
 	}
 }
