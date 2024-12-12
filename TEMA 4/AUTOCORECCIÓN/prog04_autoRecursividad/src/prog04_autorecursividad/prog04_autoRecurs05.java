21089babcecd7af5f0749cc8a28ce248bbf8c412
Angel <ags120120@gmail.com>
2024-12-12 10:37:33 +0100

    save

---
+++ TEMA 4/AUTOCORECCIÓN/prog04_autoRecursividad/src/prog04_autorecursividad/prog04_autoRecurs05.java
@@ -0,0 +1,17 @@
+package prog04_autorecursividad;
+
+import java.util.Scanner;
+
+public class prog04_autoRecurs05 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        /*
+        
+        */
+        
+        
+        
+    }
+}
