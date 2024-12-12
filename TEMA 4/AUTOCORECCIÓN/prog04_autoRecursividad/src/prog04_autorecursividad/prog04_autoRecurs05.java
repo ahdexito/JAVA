52cdf85b1dea27766ba8949f8b75960f1788e5f5
Angel <ags120120@gmail.com>
2024-12-12 13:42:58 +0100

    save

--- TEMA 4/AUTOCORECCIÓN/prog04_autoRecursividad/src/prog04_autorecursividad/prog04_autoRecurs05.java
+++ TEMA 4/AUTOCORECCIÓN/prog04_autoRecursividad/src/prog04_autorecursividad/prog04_autoRecurs05.java
@@ -1,17 +1,27 @@
 package prog04_autorecursividad;
 
-import java.util.Scanner;
-
 public class prog04_autoRecurs05 {
+	
+	public static int ProductoAB (int a, int b) {
+		
+		int x;
+		if 
+	}
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
         
         /*
-        
+        5. Crea una función recursiva que calcule el producto de dos números mediante sumas
+		repe�das. Producto (a, b)
+		Por ejemplo:
+		Producto(5,3) = 5 + Producto(5, 2) = 5 + 5 + Producto(5, 1) = 5 + 5 + 5+ Producto(5, 0) = 5
+		+ 5+ 5 + 0 = 15
         */
         
-        
+        int a = 3;
+		int b = 7;
+		
+		System.out.println("El producto entre ambos número es: " + ProductoAB(a, b));
         
     }
 }
