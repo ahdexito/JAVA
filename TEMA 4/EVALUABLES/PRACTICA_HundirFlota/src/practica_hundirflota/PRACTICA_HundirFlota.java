69c556b7fafd471d6ce6d81e1ae11a6c73a30ef0
Angel <ags120120@gmail.com>
2024-12-17 09:14:46 +0100

    save

--- TEMA 4/EVALUABLES/PRACTICA_HundirFlota/src/practica_hundirflota/PRACTICA_HundirFlota.java
+++ TEMA 4/EVALUABLES/PRACTICA_HundirFlota/src/practica_hundirflota/PRACTICA_HundirFlota.java
@@ -4,28 +4,80 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class PRACTICA_HundirFlota {
+	
+	static Scanner sc = new Scanner (System.in);
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
         
+        Menu();
+		
         //ImprimirTablero();
-        GenerarTableroFacil();
-        
-        
+		
+        //GenerarTableroFacil();
+		
+		
     }
 	
 	public static String letras = "ABCDEFGHIJ";
 	
 	public static String numeros = "   0  1  2  3  4  5  6  7  8  9";
 	
-	public static char lancha = 'L', buque = 'B', acorazado = 'Z', portaaviones = 'P';
+	public static char vacio = '-', tocado = 'X', agua = 'A',
+		lancha = 'L', buque = 'B', acorazado = 'Z', portaaviones = 'P';
 	
-	// FUNCION QUE RELLENA EL TABLERO DE "AGUA" -> " - "
+	// FUNCIÓN QUE IMPRIME MENÚ
+	public static void Menu () {
+		
+		System.out.print(""
+				+ "            HUNDIR LA FLOTA\n"
+				+ "========================================\n"
+				+ "1. Fácil\n"
+				+ "2. Medio\n"
+				+ "3. Difícil\n"
+				+ "4. Personalizado\n"
+				+ "5. Salir\n\n"
+				+ "Elegir dificultad: ");
+		
+		//int opcion = sc.nextInt();
+		int opcion = 1;
+		
+		System.out.print("\n");
+		
+		OpcionesMenu(opcion);
+		}
+	
+	
+	// FUNCION QUE OPERA LAS OPCIONES DEL MENU
+	public static void OpcionesMenu (int opcion) {
+		
+		switch (opcion) {
+			
+			case 1: GenerarTableroFacil();
+			break;
+			
+//			case 2: GenerarTableroMedio();
+//			break;
+//			
+//			case 3: GenerarTableroDificil();
+//			break;
+//			
+//			case 4: GenerarTableroCustom();
+//			break;
+//			
+//			case 5: salir = true;
+//			break;
+			
+			default: System.out.println("Opción no válida. Introduce de nuevo");
+			break;
+		}
+	}
+	
+	// FUNCIÓN QUE RELLENA EL TABLERO DE "AGUA" -> " - "
 	public static char[][] RellenarAgua (char[][] tablero) {
 		
 		for (int i=0; i<10; i++) {
 			
-			Arrays.fill(tablero[i], '-');
+			Arrays.fill(tablero[i], vacio);
 		}
 		return tablero;
 	}
@@ -47,7 +99,7 @@ public class PRACTICA_HundirFlota {
 				if (j == 0) System.out.print(letras.charAt(i) + "  ");
 				
 				// Imprimir "X" si tocado, sino imprimir agua
-				if (tableroUsuario[i][j] != '-') System.out.print("X ");
+				if (tableroUsuario[i][j] != vacio) System.out.print("X ");
 				else System.out.print(tableroUsuario[i][j] + "  ");
 			}
 			System.out.print("\n");
@@ -60,9 +112,10 @@ public class PRACTICA_HundirFlota {
 		char[][] tableroFacil = new char[10][10];
 		RellenarAgua(tableroFacil);
 		
-		boolean vacio;
-		
-		
+		for (int i=0; i<10; i++) {
+			
+			GenerarLancha(tableroFacil);
+		}
 		
 		// IMPRIMIR TABLERO FÁCIL PARA PRUEBAS
 		System.out.println(numeros);
@@ -75,5 +128,44 @@ public class PRACTICA_HundirFlota {
 			}
 			System.out.print("\n");
 		}
+	}
+	
+	// FUNCION QUE SOLICITA DISPARO
+	public static void SolicitarDisparo () {
+		
+		ImprimirTablero();
+		
+		System.out.print("\nElige coordenada para disparar: ");
+		String disparo = sc.nextLine();
+		
+		
+	}
+	
+	// FUNCION QUE VERIFICA EL DISPARO
+	public static void VerificarDisparo (String disparo, char[][] tableroUsuario, char[][] tableroMaquina) {
+		
+		
+	}
+	
+	// FUNCION QUE GENERA Y COLOCA LANCHA
+	public static char[][] GenerarLancha (char[][] tablero) {
+		
+		boolean repetir = true;
+		
+		do {	
+			int aleatorioX = (int)(Math.random() * 10);
+			
+			int aleatorioY = (int)(Math.random() * 10);
+			
+			if (tablero[aleatorioX][aleatorioY] == vacio) {
+				
+				tablero[aleatorioX][aleatorioY] = lancha;
+				
+				repetir = false;
+			}
+			
+		} while (repetir);
+		
+		return tablero;
 	}
 }
