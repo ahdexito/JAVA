9ac733ad0935d6e07a5023d41b4d4852887685a5
Angel <ags120120@gmail.com>
2025-01-27 13:17:14 +0100

    save

---
+++ TEMA 5/AUTOCORRECCIÓN/APARTADO A/ejercicio_A1/src/ejercicio_a1/Ejercicio_A1.java
@@ -0,0 +1,24 @@
+package ejercicio_a1;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ejercicio_A1 {
+    
+    public static final String RESET = "\u001B[0m", MORADO = "\u001B[35m", ROJO = "\u001B[31m", AZUL = "\u001B[34m", CIAN = "\u001B[36m", VERDE = "\u001B[32m", AMARILLO = "\u001B[33m";
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        /*
+        
+        */
+        
+        
+        
+    }
+}
