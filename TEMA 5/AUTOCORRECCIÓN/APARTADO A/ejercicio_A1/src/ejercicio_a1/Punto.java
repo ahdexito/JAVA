392f53184cd35d87db373dd43f7ed42ffc109eee
Angel <ags120120@gmail.com>
2025-01-28 11:12:49 +0100

    save

--- TEMA 5/AUTOCORRECCIÓN/APARTADO A/ejercicio_A1/src/ejercicio_a1/Punto.java
+++ TEMA 5/AUTOCORRECCIÓN/APARTADO A/ejercicio_A1/src/ejercicio_a1/Punto.java
@@ -5,4 +5,17 @@ public class Punto {
 	int x;
 	
 	int y;
+	
+	public static void Imprimir(int x, int y) {
+		
+		System.out.printf("\n(%d, %d)\n", x, y);
+	}
+	
+	public void Desplaza(int dx, int dy) {
+		
+		x += dx;
+		y += dy;
+		
+		Imprimir(x, y);
+	}
 }
