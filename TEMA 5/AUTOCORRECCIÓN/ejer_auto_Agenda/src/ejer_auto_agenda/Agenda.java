26eeb258ab8cfaf40a8bbd3736dbd37f4a1dfc81
Angel <ags120120@gmail.com>
2025-02-14 10:30:11 +0100

    save

--- TEMA 5/AUTOCORRECCIÓN/ejer_auto_Agenda/src/ejer_auto_agenda/Agenda.java
+++ TEMA 5/AUTOCORRECCIÓN/ejer_auto_Agenda/src/ejer_auto_agenda/Agenda.java
@@ -2,7 +2,7 @@ package ejer_auto_agenda;
 
 public class Agenda {
 	
-	private Contacto[] agenda = new Contacto[3];
+	private Contacto[] agenda = new Contacto[10];
 	private int contador = 0;
 	
 	// AÑADIR CONTACTO A LA ÚLTIMA POSICIÓN DISPONIBLE
@@ -71,6 +71,67 @@ public class Agenda {
 		// SI NO SE ENCUENTRA EL DNI RECIBIDO EN LA AGENDA
 		System.out.println(ROJO + "\nNO SE HA ENCONTRADO EL CONTACTO CON DNI " + dni + RESET);
 		return contacto;
+	}
+	
+	// MODIFICAR DNI, MÓVIL Y DIRECCIÓN DE UN CONTACTO
+	public boolean ModificarContacto(String dni, String movil, String direccion) {
+		
+		for (int i = 0; i < this.contador; i++) {
+			
+			if (this.agenda[i].getDni().equals(dni)) {
+				
+				this.agenda[i].setMovil(movil);
+				
+				this.agenda[i].setDireccion(direccion);
+				
+				System.out.println("\n" + VERDE + "EL CONTACTO CON DNI " + dni + " SE HA MODIFICADO" + RESET);
+				
+				return true;
+			}
+		}
+		System.out.println("\n" + ROJO + "NO SE HA PODIDO MODIFICAR EL CONTACTO CON DNI " + dni + RESET);
+		
+		return false;
+	}
+	
+	// MOSTRAR CONTACTOS QUE COINCIDAN CON EL MISMO SEXO PASADO POR PARÁMETRO
+	public void MostrarContactos(char sexo) {
+		
+		boolean encontrado = false;
+		
+		System.out.print("\n" + AMARILLO + "CONTACTOS CON SEXO '" + sexo + "'" + RESET);
+		
+		for (int i = 0; i < this.contador; i++) {
+			
+			if (this.agenda[i].getSexo() == sexo) {
+				
+				this.agenda[i].MostrarInformacion();
+				
+				encontrado = true;
+			}
+		}
+		
+		if (!encontrado) System.out.println("\n\n" + ROJO + "NO SE HA ENCONTRADO NINGUNO QUE COINCIDA CON '" + sexo + "'" + RESET);
+	}
+	
+	// MOSTRAR CONCTACTOS QUE COINCIDAN CON EL MISMO NOMBRE PASADO POR PARÁMETRO
+	public void MostrarConctactos(String nombre) {
+		
+		boolean encontrado = false;
+		
+		System.out.print("\n" + AMARILLO + "CONTACTOS CON NOMBRE '" + nombre + "'" + RESET);
+		
+		for (int i = 0; i < this.contador; i++) {
+			
+			if (this.agenda[i].getNombre().equals(nombre)) {
+				
+				this.agenda[i].MostrarInformacion();
+				
+				encontrado = true;
+			}
+		}
+		
+		if (!encontrado) System.out.println("\n\n" + ROJO + "NO SE HA ENCONTRADO NINGUNO QUE COINCIDA CON '" + nombre + "'" + RESET);
 	}
 		
 	//////////////////////////////////////////////////////
--- TEMA 5/AUTOCORRECCIÓN/ejer_auto_Agenda/src/ejer_auto_agenda/Ejer_auto_Agenda.java
+++ TEMA 5/AUTOCORRECCIÓN/ejer_auto_Agenda/src/ejer_auto_agenda/Ejer_auto_Agenda.java
@@ -8,15 +8,20 @@ public class Ejer_auto_Agenda {
 		Scanner sc = new Scanner(System.in);
 		
 		// OBJETOS CONTACTO
-		Contacto c1 = new Contacto("Ángel", "García", "Smakula", "12345678T", "666555444", "C/España Nº5", 'V');
-		Contacto c2 = new Contacto("María", "García", "Vázquez", "12345678E", "600555444", "C/Francia Nº7", 'M');
-		Contacto c3 = new Contacto("Juan", "Juan", "Juan", "12345678A", "666555777", "C/Italia Nº2", 'V');
-		Contacto c4 = new Contacto("Carlos", "Juan", "López", "12345678B", "666123777", "C/Portugal Nº8", 'V');
+		Contacto c1 = new Contacto("Luis", "Hernandez", "Lopez", "12345678A", "654789321", "C/Granada Nº10", 'V');
+		Contacto c2 = new Contacto("Marta", "Gomez", "Lopez", "23456789B", "654789322", "C/Sevilla Nº3", 'M'); // Teléfono repetido
+		Contacto c3 = new Contacto("Ana", "Lopez", "Martinez", "34567890C", "678945321", "C/Malaga Nº5", 'M');
+		Contacto c4 = new Contacto("Carlos", "Hernandez", "López", "45678901D", "666123777", "C/Portugal Nº8", 'V');
+		Contacto c5 = new Contacto("Luis", "Fernandez", "Gomez", "56789012E", "654789321", "C/Granada Nº10", 'V'); // Nombre y dirección repetidos
+		Contacto c6 = new Contacto("Elena", "Sanchez", "Ruiz", "67890123F", "600700800", "C/Toledo Nº7", 'M');
+		Contacto c7 = new Contacto("Pedro", "Garcia", "Fernandez", "78901234G", "678945322", "C/Malaga Nº5", 'V'); // Teléfono y dirección repetidos
+		Contacto c8 = new Contacto("David", "Perez", "Hernandez", "89012345H", "620987654", "C/Alicante Nº12", 'V');
+		Contacto c9 = new Contacto("Marta", "Perez", "Martinez", "90123456I", "654789323", "C/Sevilla Nº3", 'M'); // Nombre, apellidos, teléfono y dirección repetidos
+		Contacto c10 = new Contacto("Javier", "Lopez", "Martinez", "01234567J", "699888777", "C/Madrid Nº6", 'V');
+
 		
 		// OBJETO ARRAY DE CONTACTOS
 		Agenda ag1 = new Agenda();
-		
-		ag1.BorrarContacto("12345678T");
 		
 		MostrarContactos(ag1);
 		
@@ -25,33 +30,50 @@ public class Ejer_auto_Agenda {
 		ag1.AnyiadirContacto(c2);
 		ag1.AnyiadirContacto(c3);
 		ag1.AnyiadirContacto(c4);
+		ag1.AnyiadirContacto(c5);
+		ag1.AnyiadirContacto(c6);
+		ag1.AnyiadirContacto(c7);
+		ag1.AnyiadirContacto(c8);
+		ag1.AnyiadirContacto(c9);
+		ag1.AnyiadirContacto(c10);
 		
 		MostrarContactos(ag1);
 		
 		// BORRAR CONTACTO
-		ag1.BorrarContacto("12345678T");
+		ag1.BorrarContacto("45678901D");
 		
 		MostrarContactos(ag1);
 		
 		// AÑADIR CONTACTO
-		ag1.AnyiadirContacto(c1);
+		ag1.AnyiadirContacto(c4);
 		
 		MostrarContactos(ag1);
 		
 		// BORRAR CONTACTO
 		ag1.BorrarContacto("12345678B");
-		ag1.BorrarContacto("12345678T");
-		
-		MostrarContactos(ag1);
-		
-		ag1.BorrarContacto("12345678T");
 		
 		MostrarContactos(ag1);
 		
 		// BUSCAR CONTACTO
-		ag1.getBuscarContacto("12345678T");
+		ag1.getBuscarContacto("12345678B");
 		
-		ag1.getBuscarContacto("12345678E").MostrarInformacion();
+		ag1.getBuscarContacto("45678901D").MostrarInformacion();
+		
+		// MODIFICAR CONTACTO POR DNI
+		ag1.ModificarContacto("01234567J", "699888777", "C/Malaga Nº5");
+		ag1.ModificarContacto("12345678T", "999888777", "C/Europa Nº1");
+		
+		MostrarContactos(ag1);
+		
+		// MOSTRAR CONTACTO POR ATRIBUTO SEXO
+		ag1.MostrarContactos('V');
+		ag1.MostrarContactos('M');
+		ag1.MostrarContactos('H');
+		
+		// MOSTRAR CONTACTO POR ATRIBUTO NOMBRE
+		ag1.MostrarConctactos("Luis");
+		ag1.MostrarConctactos("Alberto");
+		ag1.MostrarConctactos("Marta");
 	}
 	
 	// MOSTRAR CONTACTOS
