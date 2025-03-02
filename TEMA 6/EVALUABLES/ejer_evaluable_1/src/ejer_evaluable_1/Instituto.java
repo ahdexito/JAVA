c76aeebf4f36a7ef45b94e98eacd5b356ce9ad20
Ahdexito <ags120120@gmail.com>
2025-03-02 14:01:53 +0100

    save

--- TEMA 6/EVALUABLES/ejer_evaluable_1/src/ejer_evaluable_1/Instituto.java
+++ TEMA 6/EVALUABLES/ejer_evaluable_1/src/ejer_evaluable_1/Instituto.java
@@ -7,14 +7,14 @@ public class Instituto {
 	private Titular[] titulares = new Titular[tamanyo];
 	private Interino[] interinos = new Interino[tamanyo];
 	
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
-	// AÑADIR PROFESOR INTERINO A ARRAY DE INTERINOS
+	// AÑADIR PROFESOR INTERINO A ARRAY DE INTERINOS //
 	public boolean AnyadirProfesor(Interino interino) {
 
 		for (int i = 0; i < tamanyo; i++) {
 
-			if (this.interinos[i] != null) {
+			if (this.interinos[i] == null) {
 
 				this.interinos[i] = interino;
 				return true;
@@ -23,12 +23,12 @@ public class Instituto {
 		return false;
 	}
 	
-	// AÑADIR PROFESOR TITULAR A ARRAY DE TITULARES
+	// AÑADIR PROFESOR TITULAR A ARRAY DE TITULARES //
 	public boolean AnyadirProfesor(Titular titular) {
 		
 		for (int i = 0; i < tamanyo; i++) {
 			
-			if (this.titulares[i] != null) {
+			if (this.titulares[i] == null) {
 				
 				this.titulares[i] = titular;
 				return true;
@@ -41,18 +41,20 @@ public class Instituto {
 		
 		for (int i = 0; i < tamanyo; i++) {
 			
-			// BUSCAR COINCIDENCIA DEL DNI DENTRO DEL ARRAY INTERINOS 
-			if (this.interinos[i].getDni().equals(dni)) {
-				
-				this.interinos[i] = null;
-				return true;
+			// BUSCAR COINCIDENCIA DEL DNI DENTRO DEL ARRAY INTERINOS //
+			if (this.interinos[i] != null) {
+				if (this.interinos[i].getDni().equals(dni)) {
+					this.interinos[i] = null;
+					return true;
+				}
 			}
 			
-			// BUSCAR COINCIDENCIA DEL DNI DENTRO DEL ARRAY TITULARES 
-			if (this.titulares[i].getDni().equals(dni)) {
-				
-				this.titulares[i] = null;
-				return true;
+			// BUSCAR COINCIDENCIA DEL DNI DENTRO DEL ARRAY TITULARES //
+			if (this.interinos[i] != null) {
+				if (this.titulares[i].getDni().equals(dni)) {
+					this.titulares[i] = null;
+					return true;
+				}
 			}
 		}
 		return false;
@@ -74,7 +76,7 @@ public class Instituto {
 
 	public void MostrarInformacion() {
 		
-		// MOSTRAR INFORMACIÓN DE INTERINOS
+		// MOSTRAR INFORMACIÓN DE INTERINOS //
 		for (int i = 0; i < (tamanyo); i++) {
 			
 			if (this.interinos[i] != null) {
@@ -83,7 +85,7 @@ public class Instituto {
 			}
 		}
 		
-		// MOSTRAR INFORMACIÓN DE TITULARES
+		// MOSTRAR INFORMACIÓN DE TITULARES //
 		for (int i = 0; i < (tamanyo); i++) {
 			
 			if (this.titulares[i] != null) {
@@ -93,7 +95,7 @@ public class Instituto {
 		}
 	}
 	
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	public Instituto() {
 	}
@@ -104,7 +106,7 @@ public class Instituto {
 		this.interinos = interinos;
 	}
 	
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	public String getNombre() {
 		return nombre;
@@ -128,5 +130,13 @@ public class Instituto {
 
 	public void setInterinos(Interino[] interinos) {
 		this.interinos = interinos;
+	}
+
+	public int getTamanyo() {
+		return tamanyo;
+	}
+
+	public void setTamanyo(int tamanyo) {
+		this.tamanyo = tamanyo;
 	}
 }
