d68d560ad09e6de9241fd4d7a162aaf28da6d485
Ahdexito <ags120120@gmail.com>
2025-03-22 16:18:32 +0100

    save

---
+++ PROYECTO INTERMODULAR/script_carga_asignacion_proyectos.sql
@@ -0,0 +1,37 @@
+INSERT cliente (id, DNI, nombre, apellido1, apellido2, email, telefono)
+	VALUES (null, '11111111A', 'Sebastián', 'Ramírez', 'Gómez', 's.ramgom@gmail.com', '611222333');
+
+INSERT cliente (id, DNI, nombre, apellido1, apellido2, email, telefono)
+	VALUES (null, '22222222B', 'Alba', 'Pérez', 'Delgado', 'a.perdel@gmail.com', '622333444');
+
+INSERT cliente (id, DNI, nombre, apellido1, apellido2, email, telefono)
+	VALUES (null, '33333333C', 'Carlos', 'Méndez', 'Rojas', 'c.menroj@gmail.com', '633444555');
+
+INSERT cliente (id, DNI, nombre, apellido1, apellido2, email, telefono)
+	VALUES (null, '44444444D', 'Ana', 'Beltrán', 'Suárez', 'a.belsua@gmail.com', '644555666');
+
+INSERT cliente (id, DNI, nombre, apellido1, apellido2, email, telefono)
+	VALUES (null, '55555555E', 'Luis', 'Ramírez', 'Ortega', 'l.ramort@gmail.com', '655666777');
+
+INSERT cliente (id, DNI, nombre, apellido1, apellido2, email, telefono)
+	VALUES (null, '66666666F', 'María', 'Fernández', 'Pino', 'm.ferpin@gmail.com', '666777888');
+
+INSERT cliente (id, DNI, nombre, apellido1, apellido2, email, telefono)
+	VALUES (null, '77777777G', 'Javier', 'Torres', 'Guzmán', 'j.torguz@gmail.com', '677888999');
+
+INSERT cliente (id, DNI, nombre, apellido1, apellido2, email, telefono)
+	VALUES (null, '88888888H', 'Elena', 'Castro', 'Vega', 'e.casveg@gmail.com', '688999111');
+
+INSERT cliente (id, DNI, nombre, apellido1, apellido2, email, telefono)
+	VALUES (null, '99999999I', 'Pedro', 'Morales', 'Quintana', 'p.morqui@gmail.com', '699111222');
+
+INSERT cliente (id, DNI, nombre, apellido1, apellido2, email, telefono)
+	VALUES (null, '11111111J', 'Sofía', 'Herrera', 'Lozano', 's.herloz@gmail.com', '666111222');
+
+INSERT cliente (id, DNI, nombre, apellido1, apellido2, email, telefono)
+	VALUES (null, '22222222K', 'Fernando', 'Aguilar', 'Domínguez', 'f.agudom@gmail.com', '666222333');
+
+INSERT cliente (id, DNI, nombre, apellido1, apellido2, email, telefono)
+	VALUES (null, '33333333L', 'Valeria', 'Navarro', 'Espinosa', 'v.navesp@gmail.com', '666333444');
+
+SELECT * FROM cliente;
