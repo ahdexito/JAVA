5a4a71c620f833070d0b3e4a8b7abe04919943be
Angel <ags120120@gmail.com>
2025-04-03 10:27:03 +0200

    save

--- TEMA 7/ejer_auto_3/src/ejer_auto_3/Cliente.java
+++ TEMA 7/ejer_auto_3/src/ejer_auto_3/Cliente.java
@@ -1,10 +1,10 @@
 package ejer_auto_3;
 
-import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Cliente {
 
---
+++ TEMA 7/ejer_auto_7/src/ejer_auto_7/Main.java
@@ -0,0 +1,5 @@
+package ejer_auto_7;
+
+public class Main {
+
+}
